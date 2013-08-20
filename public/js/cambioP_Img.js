b0af4322badd023f20f682a54d93f14ba1406b1e
joc <jc_perezvaldez@hotmail.com>
2013-08-20 02:12:13 -0500

    multimedios

---
+++ public/js/cambioP_Img.js
@@ -0,0 +1,66 @@
+var i=1;
+function cambioP_uno(){
+	if (i==1)
+	{ 
+	document.getElementById("p_uno").src="img/menu/pWebB.png";
+	i=2;
+	}
+	else
+	{
+	document.getElementById("p_uno").src="img/menu/pWebA.png";
+	i=1;
+	}
+}
+
+function cambioP_dos(){
+	if (i==1)
+	{ 
+	document.getElementById("p_dos").src="img/menu/pVidB.png";
+	i=2;
+	}
+	else
+	{
+	document.getElementById("p_dos").src="img/menu/pVidA.png";
+	i=1;
+	}
+}
+
+function cambioP_m5(){
+	if (i==1)
+	{ 
+	document.getElementById("p_m5").src="img/menu/pAniB.png";
+	i=2;
+	}
+	else
+	{
+	document.getElementById("p_m5").src="img/menu/pAniA.png";
+	i=1;
+	}
+}
+
+function cambioP_m6(){
+	if (i==1)
+	{ 
+	document.getElementById("p_m6").src="img/menu/pRetB.png";
+	i=2;
+	}
+	else
+	{
+	document.getElementById("p_m6").src="img/menu/pRetA.png";
+	i=1;
+	}
+}
+
+function cambioP_m7(){
+	if (i==1)
+	{ 
+	document.getElementById("p_m7").src="img/menu/pIntB.png";
+	i=2;
+	}
+	else
+	{
+	document.getElementById("p_m7").src="img/menu/pIntA.png";
+	i=1;
+	}
+}
+
---
+++ public/js/multimedia.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	$("#vid").click(function() {
+					$.fancybox.open({
+						href : 'multimedia/videos',
+						type : 'iframe',
+						padding : 8 //borderline size
+					});
+				});
+				
+	$("#ani").click(function() {
+					$.fancybox.open({
+						href : 'multimedia/animaciones',
+						type : 'iframe',
+						padding : 8 //borderline size
+					});
+				});
+				
+	$("#Int").click(function() {
+					$.fancybox.open({
+						href : 'multimedia/interactivos',
+						type : 'iframe',
+						padding : 8 //borderline size
+					});
+				});
+
+});
